15337ab669834cfbee2518db1005707da48a174f
Preethi <preethivaid@gmail.com>
2019-02-20 21:29:45 -0500

    Update tests to use expect().toBe() and edit drawing functions accordingly

--- src/svgHelpers/drawing.js
+++ src/svgHelpers/drawing.js
@@ -1,30 +1,23 @@
-export const absMovePen = (pathString, movePoint) => {
-  return `${pathString} M ${movePoint.x} ${movePoint.y}`;
+export const absMovePen = (pathString, endPoint) => {
+  return `${pathString} M ${endPoint.x} ${endPoint.y}`;
 }
 
 // relMovePen has strange behavior if its called on a function with closePath called.
-export const relMovePen = (pathString, movePoint) => {
-  return `${pathString} m ${movePoint.x} ${movePoint.y}`;
+export const relMovePen = (pathString, endPoint) => {
+  return `${pathString} m ${endPoint.x} ${endPoint.y}`;
 }
 
-export const drawAbsLine = (pathString, movePoint) => {
-  return `${pathString} L ${movePoint.x} ${movePoint.y}`;
+export const drawAbsLine = (pathString, endPoint) => {
+  return `${pathString} L ${endPoint.x} ${endPoint.y}`;
 }
 
-export const drawRelLine = (pathString, movePoint) => {
-  return `${pathString} l ${movePoint.x} ${movePoint.y}`;
+export const drawRelLine = (pathString, endPoint) => {
+  return `${pathString} l ${endPoint.x} ${endPoint.y}`;
 }
 
 // This function draws an abs quadratic bezier with a specified control point (instead of specifying the control point params)
-export const drawAbsBezAbsPoint = (pathString, movePoint, controlPoint) => {
-  return `${pathString} Q ${controlPoint.x} ${controlPoint.y} ${movePoint.x} ${movePoint.y}`;
-}
-
-// Calculates a control point by first specifying how far along the line (made by startPoint and movePoint) to go, and then 
-// how far to translate in x and y
-export const drawAbsBezRelPoint = (pathString, movePoint, startPoint, fractionAlongLine, controlPointXMove, controlPointYMove) => {
-  const controlPoint = {x: ((startPoint.x + (movePoint.x - startPoint.x) * fractionAlongLine)) + controlPointXMove, y: ((startPoint.y + (movePoint.y - startPoint.y) * fractionAlongLine)) + controlPointYMove};
-  return drawAbsBezAbsPoint(pathString, controlPoint, movePoint)
+export const drawAbsBez = (pathString, endPoint, controlPoint) => {
+  return `${pathString} Q ${controlPoint.x} ${controlPoint.y} ${endPoint.x} ${endPoint.y}`;
 }
 
 export const closePath = (pathString) => {
--- src/tests/svgHelpers.drawing.test.js
+++ src/tests/svgHelpers.drawing.test.js
@@ -2,82 +2,66 @@ import { absMovePen,
 		 relMovePen,
 		 drawAbsLine,
 		 drawRelLine,
-		 drawAbsBezAbsPoint,
+		 drawAbsBez,
 		 drawAbsBezRelPoint,
 		 closePath } from './../svgHelpers/drawing'
 
 it('moves pen (absolute)', () => {
 	let pathString = '';
-	let movePoint = {x: 10, y: 10};
-	let newPathString = absMovePen(pathString, movePoint);
-	newPathString = 'M 10 10';
+	let endPoint = {x: 10, y: 10};
+	let newPathString = absMovePen(pathString, endPoint);
+	expect(newPathString).toBe(' M 10 10');
 });
 
 it('moves pen (relative)', () => {
 	let pathString = '';
-	let movePoint = {x: 10, y: 10};
-	let newPathString = relMovePen(pathString, movePoint);
-	newPathString = 'm 10 10';
+	let endPoint = {x: 10, y: 10};
+	let newPathString = relMovePen(pathString, endPoint);
+	expect(newPathString).toBe(' m 10 10');
 });
 
 it('draws line (absolute)', () => {
 	let pathString = '';
-	let movePoint = {x: 10, y: 10};
-	let newPathString = drawAbsLine(pathString, movePoint);
-	newPathString = 'L 10 10';
+	let endPoint = {x: 10, y: 10};
+	let newPathString = drawAbsLine(pathString, endPoint);
+	expect(newPathString).toBe(' L 10 10');
 });
 
 it('draws line (relative)', () => {
 	let pathString = '';
-	let movePoint = {x: 10, y: 10};
-	let newPathString = drawRelLine(pathString, movePoint);
-	newPathString = 'l 10 10';
+	let endPoint = {x: 10, y: 10};
+	let newPathString = drawRelLine(pathString, endPoint);
+	expect(newPathString).toBe(' l 10 10');
 });
 
 it('draws bezier with absolute control point (absolute bezier)', () => {
 	let pathString = '';
-	let movePoint = {x: 10, y: 10};
+	let endPoint = {x: 10, y: 10};
 	let controlPoint = {x: 3, y: 3};
-	let newPathString = drawAbsBezAbsPoint(pathString, movePoint, controlPoint);
-	newPathString = 'Q 3 3 10 10';
-});
-
-it('draws bezier with relative control point (absolute bezier)', () => {
-	let pathString = '';
-	let movePoint = {x: 10, y: 10};
-	let startPoint = {x: 10, y: 10};
-	let fractionAlongLine = .5;
-	let controlPointXMove = 1;
-	let controlPointYMove = 1;
-	let newPathString = drawAbsBezRelPoint(movePoint, startPoint, fractionAlongLine, controlPointXMove, controlPointYMove);
-	newPathString = 'Q 6 6 10 10';
+	let newPathString = drawAbsBez(pathString, endPoint, controlPoint);
+	expect(newPathString).toBe(' Q 3 3 10 10');
 });
 
 it('closes path', () => {
 	let pathString = 'L 0 0 L 10 10 L 0 10';
-	let movePoint = {x: 10, y: 10};
-	let newPathString = closePath(pathString, movePoint);
-	newPathString = 'L 0 0 L 10 10 L 0 10 Z';
+	let endPoint = {x: 10, y: 10};
+	let newPathString = closePath(pathString, endPoint);
+	expect(newPathString).toBe('L 0 0 L 10 10 L 0 10 Z');
 });
 
 it('creates svg with all drawing functions', () => {
 	let pathString = '';
-	let movePoint = {x: 10, y: 10};
-	pathString = absMovePen(pathString, movePoint);
-	pathString = relMovePen(pathString, movePoint);
-	pathString = drawAbsLine(pathString, movePoint);
-	pathString = drawRelLine(pathString, movePoint);
+	let endPoint = {x: 10, y: 10};
+	pathString = absMovePen(pathString, endPoint);
+	pathString = relMovePen(pathString, endPoint);
+	pathString = drawAbsLine(pathString, endPoint);
+	pathString = drawRelLine(pathString, endPoint);
 
 	let controlPoint = {x: 3, y: 3};
-	pathString = drawAbsBezAbsPoint(pathString, movePoint, controlPoint);
-	
-	let startPoint = {x: 10, y: 10};
-	let fractionAlongLine = .5;
-	let controlPointXMove = 1;
-	let controlPointYMove = 1;
-	pathString = drawAbsBezRelPoint(movePoint, startPoint, fractionAlongLine, controlPointXMove, controlPointYMove);
-	pathString = closePath(pathString, movePoint);
-	pathString = 'M 10 10 m 10 10 L 10 10 l 10 10 Q 3 3 10 10 Q 6 6 10 10 Z';
+	pathString = drawAbsBez(pathString, endPoint, controlPoint);
+
+	pathString = closePath(pathString, endPoint);
+	expect(pathString).toBe(' M 10 10 m 10 10 L 10 10 l 10 10 Q 3 3 10 10 Z');
 });
 
 
