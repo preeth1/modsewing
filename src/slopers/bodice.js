5c68fe5ebc3f0d2b10fc256277dfbf1f097db46a
Preethi <preethivaid@gmail.com>
2019-02-23 23:56:35 -0500

    Fix variable names in code from old repo

--- src/slopers/bodice.js
+++ src/slopers/bodice.js
@@ -21,19 +21,6 @@ import { t0FromTQuadraticBezier,
 
 export const front = (size) => {
 
-  const a = calculateLineToLineIntersection(
-    {x: 0, y: 0}, 
-    {x: 10, y: 10}, 
-    {x: 10, y: 0}, 
-    {x: 0, y: 10});
-  const b = calculateLineToBezierIntersection(
-    {x: 0, y: 0}, 
-    {x: 10, y: 10}, 
-    {x: 10, y: 0}, 
-    {x: 0, y: 10},
-    {x: 5, y: 7}, 
-    );
-
   const convertedMeasurements = convertMeasurements(STANDARD_MEASUREMENTS[size]);
   const frontCoordinates = calculateFrontCoordinates(STANDARD_MEASUREMENTS[size], convertedMeasurements);
 
@@ -51,56 +38,58 @@ export const front = (size) => {
 
 
 export const calculateFrontCoordinates = (measurements, convertedMeasurements) => {
-  const waistDartWidth = calculateWaistDart(Math.abs(measurements.waist - measurements.lowHip));
-  const shoulderDartWidth = calculateArmholeShoulderCenterFrontDart(convertedMeasurements.cup);
-  const sideDartWidth = calculateSideDart(convertedMeasurements.cup);
+  const cm = convertedMeasurements;
+  const waistDartWidth = calculateWaistDart(measurements.waist, measurements.lowHip);
+  const shoulderDartWidth = calculateArmholeShoulderCenterFrontDart(cm.cup);
+  const sideDartWidth = calculateSideDart(cm.cup);
   const ease = 3/8;
   const shoulderDartMove = 1/4;
-  const highHipFromBottom = measurements.lowHipDepth - measurements.highHipDepth;
+  const highHipFromBottom = cm.hip.low.depth - cm.hip.high.depth;
 
   let front = {};
   front.x0 = 0;
-  front.x1 = measurements.neck.front + shoulderDartMove;
-  front.x2 = (measurements.figureBreadth - waistDartWidth)/2;
+  front.x1 = cm.neck.front + shoulderDartMove;
+  front.x2 = (cm.figureBreadth - waistDartWidth)/2;
 
   // ---Shoulder values
-  const originalShoulderHeight = (measurements.frontNeck + 1/8)/2;
-  const originalShoulderWidth = Math.sqrt(Math.pow(measurements.shoulder + shoulderDartWidth, 2) - Math.pow(originalShoulderHeight, 2));
+  const originalShoulderHeight = (cm.frontNeck + 1/8)/2;
+  const originalShoulderWidth = Math.sqrt(Math.pow(cm.shoulder + shoulderDartWidth, 2) - Math.pow(originalShoulderHeight, 2));
 
-  front.x5 = measurements.figureBreadth/2;
-  front.x6 = (measurements.figureBreadth + waistDartWidth)/2;
+  front.x5 = cm.figureBreadth/2;
+  front.x6 = (cm.figureBreadth + waistDartWidth)/2;
 
-  front.x10 = measurements.crossFront + shoulderDartWidth + 1/8;
+  front.x10 = cm.cross.front + shoulderDartWidth + 1/8;
 
-  front.x13 = measurements.waist.back + waistDartWidth + ease;
+  front.x13 = cm.waist.back + waistDartWidth + ease;
 
-  front.x18 = measurements.bost.front;
-  front.x19 = measurements.highHip.back + ease;
-  front.x20 = measurements.lowHip.back + ease;
+  front.x18 = cm.bust.front;
+  front.x19 = cm.hip.high.back + ease;
+  front.x20 = cm.hip.low.back + ease;
 
   front.y0 = 0;
   front.y1 = highHipFromBottom;
-  front.y2 = measurements.lowHip.depth - 1/2;
-  front.y3 = measurements.lowHip.depth;
+  front.y2 = cm.hip.low.depth - 1/2;
+  front.y3 = cm.hip.low.depth;
 
-  front.y5 = measurements.lowHip.depth + 3;
+  front.y5 = cm.hip.low.depth + 3;
 
-  front.y14 = measurements.lowHip.depth + measurements.length.front - 3/8;
+  front.y14 = cm.hip.low.depth + cm.length.front - 3/8;
 
-  front.y18 = measurements.lowHip.depth + measurements.length.front + measurements.neck.front + 1/8;
+  front.y18 = cm.hip.low.depth + cm.length.front + cm.neck.front + 1/8;
 
   front.x16 = front.x13;
 
-  let sideHeight = Math.sqrt(Math.pow(measurements.side + sideDartWidth, 2) - Math.pow(front.x18 - front.x13, 2));
+  let sideHeight = Math.sqrt(Math.pow(cm.side + sideDartWidth, 2) - Math.pow(front.x18 - front.x13, 2));
 
-  front.y9 = measurements.lowHip.depth + sideHeight;
+  front.y9 = cm.hip.low.depth + sideHeight;
 
   front.y12 = front.y14 - 3;
 
   // Side dart values
   const armholeCenterY = front.y9 + (front.y12 - front.y9)/2;
   front.y10 = armholeCenterY - sideDartWidth/(2*Math.sqrt(2));
-  front.y11 = armholeCenterY + sideDartWidth/(2*Math.sqrt(2));;
+  front.y11 = armholeCenterY + sideDartWidth/(2*Math.sqrt(2));
+  debugger
   front.x11 = calculateLineToBezierIntersection({x: -100, y: front.y11},
                                                 {x: 100, y: front.y11},
                                                 {x: front.x18, y: front.y9},
@@ -117,7 +106,7 @@ export const calculateFrontCoordinates = (measurements, convertedMeasurements) =
                                                     {x: front.x10, y: front.y12});
   const shoulderBottomPoint = calculateCoordAlongLine({x: front.x10, y: front.y12},
                                                       {x: front.x1 + originalShoulderWidth, y: front.y18 - originalShoulderHeight},
-                                                      measurements.backArmhole - armholeBottomLength);
+                                                      cm.armhole.back - armholeBottomLength);
   front.y15 = shoulderBottomPoint.y;
   front.x14 = shoulderBottomPoint.x;
 
@@ -132,7 +121,7 @@ export const calculateFrontCoordinates = (measurements, convertedMeasurements) =
 
   front.y13 = front.y17 - 3;
 
-  front.y7 = front.y14 - Math.sqrt(Math.pow(measurements.figureLength, 2) - Math.pow(measurements.figureBreadth/2, 2));
+  front.y7 = front.y14 - Math.sqrt(Math.pow(cm.figureLength, 2) - Math.pow(cm.figureBreadth/2, 2));
 
   front.x17 = calculateLineToBezierIntersection({x: -100, y: front.y7 + sideDartWidth/2},
                                          {x: 100, y: front.y7 + sideDartWidth/2},
