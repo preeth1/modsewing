705f9163de7bba2ac43756d5aa49b9efef7309e5
Preethi <preethivaid@gmail.com>
2019-02-24 14:39:00 -0500

    Make y values of drawing fns negative

--- src/svgHelpers/drawing.js
+++ src/svgHelpers/drawing.js
@@ -1,21 +1,21 @@
 export const absMovePen = (end) => {
-  return [{command: 'M', end: {x: end.x, y: end.y}, absolute: true}];
+  return [{command: 'M', end: {x: end.x, y: -end.y}, absolute: true}];
 }
 
 // relMovePen has strange behavior if its called on a function with closePath called.
 export const relMovePen = (end) => {
-  return [{command: 'm', end: {x: end.x, y: end.y}, absolute: false}];
+  return [{command: 'm', end: {x: end.x, y: -end.y}, absolute: false}];
 }
 
 export const drawAbsLine = (end) => {
-  return [{command: 'L', end: {x: end.x, y: end.y}, absolute: true}];
+  return [{command: 'L', end: {x: end.x, y: -end.y}, absolute: true}];
 }
 
 export const drawRelLine = (end) => {
-  return [{command: 'l', end: {x: end.x, y: end.y}, absolute: false}];
+  return [{command: 'l', end: {x: end.x, y: -end.y}, absolute: false}];
 }
 
 // This function draws an abs quadratic bezier with a specified control point (instead of specifying the control point params)
 export const drawAbsBez = (end, control) => {
-  return [{command: 'Q', control: {x: control.x, y: control.y}, end: {x: end.x, y: end.y}, absolute: true}];
+  return [{command: 'Q', control: {x: control.x, y: -control.y}, end: {x: end.x, y: -end.y}, absolute: true}];
 }
