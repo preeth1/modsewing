b4cf5bc9abe37bf1f00d0192699e8f7907734158
Preethi <preethivaid@gmail.com>
2019-05-10 18:59:04 -0400

    Get rid of first page - go straight to tutorial

--- src/App.js
+++ src/App.js
@@ -25,16 +25,10 @@ class App extends Component {
             <img className="TitleImage" src={titleImage} alt="Modsewing"/>
           </div>
           <Route exact path='/' render={(props) =>
-            <MeasurementsPage measurements={this.state.measurements}
-            history={history}
-            displayImage={this.state.displayImage}
-            imageDescription={this.state.imageDescription}/>}
+            <TutorialPage history={history} measurements={this.state.measurements}/>}
           />
           <Route exact path='/generatePattern' render={(props) =>
             <GeneratePage measurements={this.state.measurements}/>}
-          />
-          <Route exact path='/getMeasurements' render={(props) =>
-            <TutorialPage history={history} measurements={this.state.measurements}/>}
           />
         </div>
       </Router>
@@ -43,56 +37,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-class MeasurementsPage extends Component {
-
-  generateMeasurementLabels = () => {
-    let measurementLabels = []
-    _.each(get_measurements({}), (measurementInfo, measurementName) => {
-      measurementLabels.push(<label className="MeasurementLabel">
-                  { measurementName }:
-                  <input type="text"
-                  value={this.props.measurements[measurementName].measurement}
-                  onChange={(event) => this.handleChange(measurementName, event)}
-                  onFocus = {() => this.handleFocus(measurementInfo)} />
-                </label>
-                )
-    })
-    return measurementLabels
-  }
-
-  generatePattern = (event) => {
-    this.props.history.replace('/generatePattern')
-  }
-
-  measurementTutorialButtonClicked = (event) => {
-    this.props.history.replace('/getMeasurements')
-  }
-
-render () {
-    return (
-      <div className="Measurements">
-        <div className="ContentPanelMeasurements">
-          <div className="MeasurementButton measurementTutorialButton" onClick={this.measurementTutorialButtonClicked}>
-          Take your measurements
-          </div>
-          <div className="MeasurementButton MeasurementBuiltInButton">
-          Use a built-in measurement
-            <div className="BuiltInButtonPanel">
-              <div className="BuiltInButton" onClick={this.generatePattern}>
-              S
-              </div>
-              <div className="BuiltInButton" onClick={this.generatePattern}>
-              M
-              </div>
-              <div className="BuiltInButton" onClick={this.generatePattern}>
-              L
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      )
-  }
-}
