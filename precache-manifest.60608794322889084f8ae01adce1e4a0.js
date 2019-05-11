5e00cea6114c58847ed75b38ea54e81d0a24f92a
Preethi <preethivaid@gmail.com>
2019-05-11 16:39:58 -0400

    Updates

---
+++ precache-manifest.60608794322889084f8ae01adce1e4a0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf60792aad5e67156911",
+    "url": "/static/css/main.f154c8dc.chunk.css"
+  },
+  {
+    "revision": "bf60792aad5e67156911",
+    "url": "/static/js/main.bf60792a.chunk.js"
+  },
+  {
+    "revision": "cc41abd33de55674b5ff",
+    "url": "/static/js/1.cc41abd3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "198fa2053a23237411122b5e76618107",
+    "url": "/static/media/title.198fa205.svg"
+  },
+  {
+    "revision": "b65eb639a38ad792ba687923c7017d55",
+    "url": "/static/media/dressform.b65eb639.svg"
+  },
+  {
+    "revision": "3547d7bb470325a9bce8e8ef8f705fd9",
+    "url": "/static/media/shoulder.3547d7bb.svg"
+  },
+  {
+    "revision": "664e9664b39d73d75883e79a1f49d4e8",
+    "url": "/static/media/neck.664e9664.svg"
+  },
+  {
+    "revision": "cc12c9432ce83cab71f72b4526ee07aa",
+    "url": "/static/media/waist.cc12c943.svg"
+  },
+  {
+    "revision": "c768657676ddb710bc42cd8374fb0df0",
+    "url": "/static/media/frontLength.c7686576.svg"
+  },
+  {
+    "revision": "54e89cb30468f286c8a7dfe012ca9dbb",
+    "url": "/static/media/side.54e89cb3.svg"
+  },
+  {
+    "revision": "27dbde610e247e67dc72e167aa506fe2",
+    "url": "/static/media/highHip.27dbde61.svg"
+  },
+  {
+    "revision": "a0c92ddc52583f8b189bf29fb9da0232",
+    "url": "/static/media/highHipDepth.a0c92ddc.svg"
+  },
+  {
+    "revision": "95c4ab8bcb8e0252ffc80ebc74047786",
+    "url": "/static/media/lowHip.95c4ab8b.svg"
+  },
+  {
+    "revision": "4e9e5f386cb6a29ae87f903d58c8ad9a",
+    "url": "/static/media/lowHipDepth.4e9e5f38.svg"
+  },
+  {
+    "revision": "059e825ef3b96c0f5c094ff40fcb21c5",
+    "url": "/static/media/backLength.059e825e.svg"
+  },
+  {
+    "revision": "dfe9cdefa9585618938114482a6a615b",
+    "url": "/static/media/figureLength.dfe9cdef.svg"
+  },
+  {
+    "revision": "3ba60b1bd9e09832ddd53e138f8d5b4b",
+    "url": "/static/media/figureBreadth.3ba60b1b.svg"
+  },
+  {
+    "revision": "dbd5a5329319aa6cd269b55919321d12",
+    "url": "/static/media/crossFront.dbd5a532.svg"
+  },
+  {
+    "revision": "67e23ba6c8c3e10be4e83439e454278a",
+    "url": "/static/media/crossBack.67e23ba6.svg"
+  },
+  {
+    "revision": "9f415f7ad9ce589360f6ceb6fb4c26c9",
+    "url": "/static/media/bust.9f415f7a.svg"
+  },
+  {
+    "revision": "e2d0ef3ae4e72fa67007c5fe533a301d",
+    "url": "/static/media/underBust.e2d0ef3a.svg"
+  },
+  {
+    "revision": "69760c2058efb9bcd41f4c67d1360d2f",
+    "url": "/static/media/armhole.69760c20.svg"
+  },
+  {
+    "revision": "9761d0923cbc4c948b70acd76f142da6",
+    "url": "/index.html"
+  }
+];
